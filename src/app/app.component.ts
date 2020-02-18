bb8bbb159d4304c124a9ecaa8cbb1b06368ed80b
DanielBSky <danyabrodsky@gmail.com>
2020-02-18 11:24:59 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'client';
+  url = 'http://localhost:8000/api/login/github';
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  sendData() {
+    // const httpOptions = {
+    //   headers: new HttpHeaders({
+    //     'Access-Control-Allow-Origin':'*',
+    //   })
+    // };
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json',
+        'Access-Control-Allow-Origin': '*',
+      })
+    };
+    const obj = {
+      driver: 'github'
+    };
+    this.http.post(this.url, obj).subscribe();
+  }
 }
